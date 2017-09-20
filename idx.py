679a9f2dbfe3966687ee4443184a1f90c5669fd6
spartakos87 <serepasf@yahoo.gr>
2017-09-20 15:43:03 +0300

    include the paths

---
+++ idx.py
@@ -0,0 +1,37 @@
+lst =[["SS_Production",[17,27],"EU28-A"],
+["SS_NetImports",[28,34],"EU28-A"	],
+["Gross_Electricity",[38,48],"EU28-A"	],
+["Net_Generation",[49,64],"EU28-A"	],
+["Transport_Passenger",[5,10],"EU28-B"],
+["Transport_Freight",[11,14],"EU28-B"	],
+["Transport_Energy",[11,14],"EU28-B"	],
+["Demand_by_sector",[32,38],"EU28-B"	],
+["Demand_by_fuel",[39,46],"EU28-B"	],
+["GHG_emissions",[56,58],"EU28-B"	],
+["CO2_emissions",[59,65],"EU28-B"	],
+["RES_Gross",[76,79],"EU28-B"	],
+["Markets",[81,83],"EU28-B"	]
+
+
+
+
+]
+
+
+lst2 =[["SS_Production",[6,16],"EU28-S"],
+["SS_NetImports",[18,24],"EU28-S"       ],
+["Gross_Electricity",[37,47],"EU28-I"   ],
+["Net_Generation",[48,63],"EU28-I"      ],
+["Transport_Passenger",[76,81],"EU28-I"],
+["Transport_Freight",[83,86],"EU28-I"   ],
+["Transport_Energy",[88,94],"EU28-I"    ],
+["Demand_by_sector",[63,69],"EU28-S"    ],
+["Demand_by_fuel",[71,78],"EU28-S"      ],
+["CO2_emissions",[85,91],"EU28-S"       ],
+
+
+
+
+]
+
+paths=["new/D40R30E30","new/D40R33E33","new/D40R35E35","senaria/D40R27E27c","senaria/D40R27E30c","senaria/D40R27E33b","remainingfiles/D40R27E35b","remainingfiles/D40R27E40b"]
